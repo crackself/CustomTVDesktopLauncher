3ea5a7fb27997f5f3417dc49fe237bfcb7c55039
crackself <crackself@gmail.com>
2025-02-02 14:26:59 +0800

    Update HomePageActivity.java

--- app/src/main/java/com/coocaa/homepage/vast/HomePageActivity.java
+++ app/src/main/java/com/coocaa/homepage/vast/HomePageActivity.java
@@ -198,7 +198,7 @@ public class HomePageActivity extends AppCompatActivity {
     private void launchTv() {
 //        com.dianshijia.newlive
         PackageManager packageManager = getPackageManager();
-        Intent intent = packageManager.getLaunchIntentForPackage("com.tianci.tv");
+        Intent intent = packageManager.getLaunchIntentForPackage("com.skyworth.tv_browser");
         launchByIntent(intent, "电视");
 
 
